c21621b80cb2a51d58d5c40d6b1f8086d1b5a71a
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2024-07-30 22:08:41 +0530

    DOM project is done

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.getElementById("container").style.color = "blue";
+document.getElementById("container").style.padding = "10px";
+
+document.getElementById("container").style.backgroundColor = "yellow";
+
+document.getElementById("container").style.fontSize = "18px";
+document.getElementById("container").style.margin = "20px";
+
+document.getElementById("container").style.fontWeight = "bold";
+
+document.getElementById("container").style.height = "200px";
+document.getElementById("container").style.width = "300px";
